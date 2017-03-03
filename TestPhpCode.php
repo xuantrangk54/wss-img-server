5cc2130113e87e34d895c352a089408fde53d705
xuantrangk54 <xuantrangk54@gmail.com>
2017-03-03 10:51:22 +0700

    TestTeamCity

--- TestPhpCode.php
+++ TestPhpCode.php
@@ -1,5 +1,25 @@
-<?php
-	         $capital = 67;
-	         print("Variable capital is $capital<br>");
-	         print("Variable CaPiTaL is $CaPiTaL<br>");
-?>
+<html>
+   <body>
+   
+      <?php
+         /* First method to create array. */
+         $numbers = array( 1, 2, 3, 4, 5);
+         
+         foreach( $numbers as $value ) {
+            echo "Value is $value <br />";
+         }
+         
+         /* Second method to create array. */
+         $numbers[0] = "one";
+         $numbers[1] = "two";
+         $numbers[2] = "three";
+         $numbers[3] = "four";
+         $numbers[4] = "five";
+         
+         foreach( $numbers as $value ) {
+            echo "Value is $value <br />";
+         }
+      ?>
+      
+   </body>
+</html>
